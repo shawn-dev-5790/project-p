c72bcc402370ad98ffd16d24ac186e6b0d6cdcdd
shawn-dev-5790 <shawn.dev.5790@gmail.com>
2023-04-09 19:09:17 +0900

    feat: :sparkles: 호ㅏ면 추가

--- src/main.ts
+++ src/main.ts
@@ -1,23 +1,17 @@
 import './style.css'
-import typescriptLogo from '/typescript.svg'
-import viteLogo from '/vite.svg'
 import { setupCounter } from './counter'
 
 document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://www.typescriptlang.org/" target="_blank">
-      <img src="${typescriptLogo}" class="logo vanilla" alt="TypeScript logo" />
-    </a>
-    <h1>Vite + TypeScript</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
+  <div class="wrap">
+    <div>
+      <button id="btnMix">mix</button>
+      <button id="btnWave">wave</button>
     </div>
-    <p class="read-the-docs">
-      Click on the Vite and TypeScript logos to learn more
-    </p>
+    <div class="grid">
+    ${Array.from({ length: 10 * 10 }, (_, i) => i)
+      .map((cell) => `<div class="cell">${cell}</div>`)
+      .join('')}
+    </div>
   </div>
 `
 
